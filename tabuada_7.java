c61a0312afff32fbf6a2758312264be23783e654
Micael Marcos Duarte da Silva <micael.mdsilva@gmail.com>
2025-11-05 14:46:45 -0300

    Ajuste na identação

--- tabuada_7.java
+++ tabuada_7.java
@@ -3,32 +3,38 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LerTabuadaDoArquivo {
+
     public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-    System.out.print("Digite o número da tabuada: ");
-         int num = input.nextInt();
-         String nomeArquivo = "tabuada_" + num + ".txt";
-         File arquivo = new File(nomeArquivo);
-         if (!arquivo.exists()) {
-        System.out.println("O arquivo " + nomeArquivo + " não existe!");
-          } else {
-               try {                                              
-                 Scanner leitorArquivo = new Scanner(arquivo);
 
-                                          System.out.println("Conteúdo do arquivo " + nomeArquivo + ":");
-                                                                                                                                    System.out.println("------------------------------------------");
+        Scanner input = new Scanner(System.in);
 
-                                                                                                                                                    while (leitorArquivo.hasNextLine()) {
-                                                                                                                                                                        String linha = leitorArquivo.nextLine();
-                                                                                                                                                                                            System.out.println(linha);
-                                                                                                                                                                                                            }
+        System.out.print("Digite o número da tabuada: ");
+        int num = input.nextInt();
 
-                                                                                                                                                                                                                            leitorArquivo.close();
-                                                                                                                                                                                                                                        } catch (FileNotFoundException e) {
-                                                                                                                                                                                                                                                        System.out.println("Erro ao ler o arquivo.");
-                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                            }
+        String nomeArquivo = "tabuada_" + num + ".txt";
+        File arquivo = new File(nomeArquivo);
 
-                                                                                                                                                                                                                                                                                    input.close();
-                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                        }
+        if (!arquivo.exists()) {
+            System.out.println("O arquivo " + nomeArquivo + " não existe!");
+        } else {
+            try {
+                Scanner leitorArquivo = new Scanner(arquivo);
+
+                System.out.println("Conteúdo do arquivo " + nomeArquivo + ":");
+                System.out.println("------------------------------------------");
+
+                while (leitorArquivo.hasNextLine()) {
+                    String linha = leitorArquivo.nextLine();
+                    System.out.println(linha);
+                }
+
+                leitorArquivo.close();
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Erro ao ler o arquivo.");
+            }
+        }
+
+        input.close();
+    }
+}
